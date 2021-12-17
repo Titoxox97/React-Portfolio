88f51392329009d14570bc0eddfe73fbb660167e
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2021-12-16 21:27:08 -0500

    setting up

---
+++ psalm/src/component/component1/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+export default function component1(prop) {
+
+
+    return (
+        <div className = "block">
+            <p>{prop.thegreating}</p>
+        </div>
+    )
+}
